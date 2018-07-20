3ce43075cc09a702cf751dbf1ebd7cabce746f82
fernando lobo <fernando.lobo.prez@gmail.com>
2018-07-20 14:55:46 -0300

    changes to index 2

--- client/src/components/Header_footer/Footer/index.js
+++ client/src/components/Header_footer/Footer/index.js
@@ -5,8 +5,6 @@ import faPhone from '@fortawesome/fontawesome-free-solid/faPhone';
 import faClock from '@fortawesome/fontawesome-free-solid/faClock';
 import faEnvelope from '@fortawesome/fontawesome-free-solid/faEnvelope';
 
-
-
 const Footer = ({data}) => {
     return (
         data.siteData ?
